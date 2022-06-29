ef4b4a23df51a2ff27624555cb84da96c351f9bc
Daniel <daniel.zsolnai@transcom.com>
2022-06-29 18:10:28 +0000

    Implement argument parsing instead stdin stdout

--- README.md
+++ README.md
@@ -1,23 +1,30 @@
 # Proto3 renumber
 
-Ever wondered why proto3 format needs all those numbers when most of the times you just end up doing sequential numbers?
-
-Worry no more this package saves you from having to manually renumber your protos just provide a file you want to renumber on the standard input and it will spit out the result to the standard output.
+Ever wondered why proto3 format needs all those numbers when most of the times you just end up doing sequential numbers? I don't know but here's a tool that let's you deal with them like they did not have numbers.
 
 ## Installing
 
 Run `npm run build` then `npm link` and you are ready to use proto3-renumber from the CLI.
 
-## Usage
+## Basic Usage
+
+Choose input file with `--input` or `-i`. Choose output file with `--output` or `-o`.
 
 ```
-proto_to_renumber.proto | proto3-renumber > renumbered_proto.proto
+proto3-renumber --input proto_to_renumber.proto --output renumbered_proto.proto
 ```
 
-Make sure to you use `--silent` if you run decide to run from npm scripts or the npm run log will be in your output.
+## Renumber all files in the current working directory
 
-**Warning:** If you want to rewrite the same file then due to the quirks of shells you will have to redirect to a program that writes to the file like `tee`
+You can also renumber all files in the current working directory by providing `--all` or `-a`
 
 ```
-proto_to_renumber.proto | proto3-renumber | tee proto_to_renumber.proto
+proto3-renumber --all
 ```
+
+## Is the renumbering destructive?
+
+
+If you skip numbers deliberately don't use this tool because that will also be renumbered.
+
+Otherwise as long as you use a sensible format it should not be destructive. You can write valid protos which will be mocked up, because it's a really crude implementation of find and replace instead of parsing the proto file itself.
--- src/index.ts
+++ src/index.ts
@@ -1,23 +1,45 @@
 #!/usr/bin/env node
 
 import fs from 'fs';
+import yargs from 'yargs';
+import chalk from 'chalk';
+import klawSync from 'klaw-sync';
 
-let input = fs.readFileSync(process.stdin.fd, 'utf-8');
+const argv = yargs(process.argv.slice(2))
+  .option('input', { alias:'i', type: 'string' })
+  .option('all', { alias:'a', type: 'boolean' })
+  .option('output', { alias: 'o', type: 'string'})
+  .parseSync();
 
-const newline = input.includes('\r') ? '\r\n' : '\n';
-
-const matches = input.matchAll(/message ([A-Za-z0-9_]+) {([^}]*)}/gm);
-
-for (const match of matches) {
-  const fields = match[2].split(newline).filter(f => !!f.trim())
-  const fieldsStr = fields.join(newline);
-  const fieldsStrTransformed = fields.map((f, i) => {
-      let rawField = f.split('=')[0].trimEnd().replace(';', '');
-      rawField += ` = ${i + 1};`;
-      return rawField;
-    }).join(newline);
-  const newMatch = match[0].replace(fieldsStr, fieldsStrTransformed);
-  input = input.replace(match[0], newMatch);
+if (argv.all) {
+  const files = klawSync('.', { nodir: true });
+  for (const file of files) {
+    renumber(file.path);
+  }
+} else if (argv.input) {
+  renumber(argv.input, argv.output);
+} else {
+  console.error(chalk.redBright('No input file provided.')
+    + '\n\nEither provide one with -i or --input or renumber all files with -a or --all.');
 }
 
-fs.writeFileSync(process.stdout.fd, input, 'utf-8')
+function renumber(inputFile: string, outputFile?: string) {
+  let input = fs.readFileSync(inputFile, 'utf-8');
+  const newline = input.includes('\r') ? '\r\n' : '\n';
+  const matches = input.matchAll(/message ([A-Za-z0-9_]+) {([^}]*)}/gm);
+
+  for (const match of matches) {
+    const fields = match[2].split(newline).filter(f => !!f.trim())
+    const fieldsStr = fields.join(newline);
+    const fieldsStrTransformed = fields.map((f, i) => {
+        let rawField = f.split('=')[0].trimEnd().replace(';', '');
+        rawField += ` = ${i + 1};`;
+        return rawField;
+      }).join(newline);
+    const newMatch = match[0].replace(fieldsStr, fieldsStrTransformed);
+    input = input.replace(match[0], newMatch);
+  }
+
+  fs.writeFileSync(outputFile ?? inputFile, input, 'utf-8')
+  console.log(chalk.greenBright(`Renumbered ${chalk.cyanBright(inputFile)}`))
+}
